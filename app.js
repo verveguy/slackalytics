c791b15bfb4a33644dc1ad6586d0f7282941e86b
Brett Adam <bpjadam@gmail.com>
2016-09-14 08:26:05 +1000

    Cleanup

--- app.js
+++ app.js
@@ -10,20 +10,26 @@ License information
 */
 
 //Set up Reqs
-var request = require('request');
-var qs = require('querystring');
-var reqpromise = require('request-promise-native');
-var ApiBuilder = require('claudia-api-builder');
+const request = require('request');
+const qs = require('querystring');
+const reqpromise = require('request-promise-native');
+const ApiBuilder = require('claudia-api-builder');
 
-var api = new ApiBuilder();
+const api = new ApiBuilder();
 
 module.exports = api;
 
-//Routes
+// ---
+// routes
+
+// not sure if we need this with claudiajs handing the boilerplate
+
 api.get('/', function(req){
   return 'here';
 });
 
+
+// 
 api.post('/collect', function(req){
   
   console.log("REQUEST:", JSON.stringify(req));
@@ -45,41 +51,43 @@ api.post('/collect', function(req){
     id:   req.post.channel_id,
     name:   req.post.channel_name
   };
-  var user = {
+  
+  const user = {
     id:   req.post.user_id,
     name: req.post.user_name
   };
-  var msgText = req.post.text;
-  var teamDomain = req.post.team_domain;
+  
+  const msgText = req.post.text;
+  const teamDomain = req.post.team_domain;
 
 
-  function searchM(regex){
-    var searchStr = msgText.match(regex);
-    if(searchStr != null){
+  function searchM(regex) {
+    let searchStr = msgText.match(regex);
+    if (searchStr != null) {
       return searchStr.length;
     }
     return 0;
   };
 
   function searchS(regex){
-    var searchStr = msgText.split(regex);
-    if(searchStr != undefined){
+    const searchStr = msgText.split(regex);
+    if (searchStr != undefined) {
       return searchStr.length;
     }
     return 0;
   };
 
 
-  var wordCount = searchS(/\s+\b/);
-  var emojiCount = searchM(/:[a-z_0-9]*:/g);
-  var exclaCount = searchM(/!/g);
-  var questionMark = searchM(/\?/g);
-  var elipseCount = searchM(/\.\.\./g);
-  var letterCount = searchM(/./g);
+  let wordCount = searchS(/\s+\b/);
+  let emojiCount = searchM(/:[a-z_0-9]*:/g);
+  let exclaCount = searchM(/!/g);
+  let questionMark = searchM(/\?/g);
+  let elipseCount = searchM(/\.\.\./g);
+  let letterCount = searchM(/./g);
 
 
   //Structure Data
-  var data = {
+  let data = {
     v:     1,
     tid:   GA_key,
     cid:   user.id,
@@ -87,35 +95,30 @@ api.post('/collect', function(req){
     cs:    "slack", // campaign source
     cd1:   user.id,
     cd2:   channel.name,
-    cd3:   msgText,
-    cd4:   user.name,
+    cd3:   user.name,
+    // cd4:   msgText,  /* using this leaks message data. caution */
     cm1:   wordCount,
     cm2:   emojiCount,
     cm3:   exclaCount,
     cm4:   letterCount,
     cm5:   elipseCount, 
-    cm6:   questionMark, //need to set up in GA
+    cm6:   questionMark,
     dh:    teamDomain+".slack.com",
-    dp:    "/"+channel.name,
-    dt:    "Slack Channel: "+channel.name,
+    dp:    "/"+channel.name,  // we treat channels like URL paths
+    dt:    "channel: " + channel.name,
     t:     "event",
     ec:   "slack: "+ channel.name + "|" + channel.id,
     ea:   "post by " + user.name,
     el:   "post by " + user.name,
-    //el:   msgText,
+    //el:   msgText, /* using this leaks message data. caution */
     ev:   1 
   };
   console.log(JSON.stringify(data));
   
   //Make Post Request to GA via Promise
-  var options = {
+  let options = {
       method: 'POST',
       uri: 'https://www.google-analytics.com/collect?'+ qs.stringify(data),
-/*      body: {
-          some: 'payload'
-      },
-      json: true // Automatically stringifies the body to JSON 
-      */
   };
 
   return reqpromise(options)
