95649e814fd13e296acaccd366c5197a454ad500
Brett Adam <bpjadam@gmail.com>
2016-11-08 23:33:46 +1100

    Attempting some cleanups / clarifications. Comments

--- app.js
+++ app.js
@@ -36,11 +36,13 @@ const api = new ApiBuilder()
 
 module.exports = api
 
+// TODO: The offtopic marker can also come via reactions (rather than text)
+// we need to handle reaction events which requires EventsAPI
+
 // ---
 // routes
 
-// not sure if we need this with claudiajs handing the boilerplate
-
+// TODO: not sure if we need this with claudiajs handing the boilerplate
 api.get('/', function (req) {
   return 'here'
 })
@@ -78,7 +80,7 @@ api.post('/collect', function (req) {
 
   function searchM (regex) {
     let searchStr = msgText.match(regex)
-    if (searchStr != null) {
+    if (searchStr !== null) {
       return searchStr.length
     }
     return 0
@@ -86,7 +88,7 @@ api.post('/collect', function (req) {
 
   function searchS (regex) {
     const searchStr = msgText.split(regex)
-    if (searchStr != undefined) {
+    if (searchStr !== undefined) {
       return searchStr.length
     }
     return 0
@@ -94,34 +96,45 @@ api.post('/collect', function (req) {
 
   let wordCount = searchS(/\s+\b/)
   let emojiCount = searchM(/:[a-z_0-9]*:/g)
+  let offtopicCount = searchM(/:batman:/g)  // Zendesk specific reaction marker
   let exclaCount = searchM(/!/g)
   let questionMark = searchM(/\?/g)
-  let elipseCount = searchM(/\.\.\./g)
+  let elipsisCount = searchM(/\.\.\./g)
   let alertCount = searchM(/<!/g)
   let urlCount = searchM(/<http/g)
 
-  // Google Analytics collect struct
+  // Google Analytics Measure Protocol collection struct
+  // see https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+
   let data = {
     v: 1,
     tid: GA_key,
-    cid: user.id,
+    t: 'event',
+    ni: 'true',
+
+    cid: user.id,  //clientID (user ID)
+    uid: user.id,  // userID
     ds: 'slack', // data source
-    cs: 'slack', // campaign source
-    cd1: user.id,
+    dh: teamDomain + '.slack.com',  // doc host
+    dp: '/' + channel.name,  // we treat channels like URL paths
+    dt: 'channel: ' + channel.name,  // doc title
+
+    // custom dimensions
+    cd1: user.name,
     cd2: channel.name,
-    cd3: user.name,
+    cd3: teamDomain,
     // cd4:   msgText,  /* using this leaks message data. caution */
+
+    // custom metrics
     cm1: wordCount,
     cm2: emojiCount,
     cm3: exclaCount,
     cm4: questionMark,
-    cm5: elipseCount,
+    cm5: elipsisCount,
     cm6: urlCount,
     cm7: alertCount,
-    dh: teamDomain + '.slack.com',
-    dp: '/' + channel.name,  // we treat channels like URL paths
-    dt: 'channel: ' + channel.name,
-    t: 'event',
+    cm8: offtopicCount,
+
     ec: 'slack: ' + channel.name + '|' + channel.id,
     ea: 'post by ' + user.name,
     el: 'post by ' + user.name,
